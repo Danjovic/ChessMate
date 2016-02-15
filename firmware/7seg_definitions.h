3c5c99dec3fd9a88d33aca02b284073054506aeb
Reinaldo <reinaldosflamino@gmail.com>
2016-02-15 11:36:41 -0200

    Create 7seg_definitions.h

---
+++ firmware/7seg_definitions.h
@@ -0,0 +1,46 @@
+
+
+
+// Definitions
+//                      7654 3210
+//                      GBDE A.FC
+#define _0    0x7b  //  0111 1011
+#define _1    0x41  //  0100 0001
+#define _2    0xf8  //  1111 1000
+#define _3    0xe9  //  1110 1001
+#define _4    0xc3  //  1100 0011
+#define _5    0xab  //  1010 1011
+#define _6    0xbb  //  1011 1011
+#define _7    0x49  //  0100 1001
+#define _8    0xfb  //  1111 1011
+#define _9    0xeb  //  1110 1011
+#define _A    0xdb  //  1101 1011
+#define _B    0xb3  //  1011 0011
+#define _C    0x3a  //  0011 1010
+#define _D    0xf1  //  1111 0001
+#define _E    0xba  //  1011 1010
+#define _F    0x9a  //  1001 1010
+#define _G    0x3b  //  0111 1011
+#define _H    0x93  //  1001 0011
+#define _I    0x01  //  0000 0001
+#define _J    0x71  //  0111 0001
+#define _K    0xd3  //  1101 0011
+#define _L    0x32  //  0011 0010
+#define _M    0x5b  //  0101 1011
+#define _N    0x91  //  1001 0001
+#define _O    0xb1  //  1011 0001
+#define _P    0xda  //  1101 1010
+#define _Q    0xcb  //  1100 1011
+#define _R    0x90  //  1001 0000
+#define _S    0xab  //  1010 1011
+#define _T    0xb2  //  1011 0010
+#define _U    0x31  //  0011 0001
+#define _V    0x73  //  0111 0011
+#define _W    0x73  //  0111 0011
+#define _X    0xd3  //  1101 0011
+#define _Y    0xe3  //  1110 0011
+#define _Z    0xf8  //  1111 1000
+#define _SPC  0x00  //  0000 0000  Space
+#define _DP   0x04  //  0000 0100  Decimal point
+                                 
+
